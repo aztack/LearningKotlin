edfb04f82c55b216c4706f12f0437bb8d70a65b9
wangweihua <aztack@163.com>
2020-11-09 20:06:37 +0800

    feat: add chaper2 section one

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
 import `Q&A`.q_and_a
 import cookbook.ch1_ranges_progressions_sequences.ch1
+import cookbook.ch2_expressive_functions_and_adjustable_interfaces.ch2
 
 fun main() {
     ch1()
+    ch2()
     q_and_a()
 }
--- src/main/kotlin/cookbook/ch2_expressive_functions_and_adjustable_interfaces/ch2.kt
+++ src/main/kotlin/cookbook/ch2_expressive_functions_and_adjustable_interfaces/ch2.kt
@@ -1,2 +1,31 @@
 package cookbook.ch2_expressive_functions_and_adjustable_interfaces
 
+import code
+import comment
+import h1
+import h2
+
+fun ch2() {
+    h1("Ch1. Expressive Functions and Adjustable Interfaces")
+    h2(arrayOf(::declaring_adjustable_functions_with_default_paramaters))
+}
+
+fun declaring_adjustable_functions_with_default_paramaters() {
+    fun calculateDisplacement(initialSpeed: Float = 0f, acceleration: Float, duration: Long = 10)
+            = initialSpeed * duration + 0.5 * acceleration * duration * duration
+    code("""
+    fun calculateDisplacement(initialSpeed: Float = 0f, acceleration: Float, duration: Long = 10)
+        = initialSpeed * duration + 0.5 * acceleration * duration * duration
+    println(calculateDisplacement(acceleration =  9.81f, duration = 1000))
+    println(calculateDisplacement(acceleration =  9.81f))
+    """)
+    run {
+        println(calculateDisplacement(acceleration =  9.81f, duration = 1000))
+        println(calculateDisplacement(acceleration =  9.81f))
+    }
+    comment("""
+    Default parameters in Kotlin can be put any where in the parameter list, not necessarily the last like TypeScript.
+    Function arguments can have different order than parameter list and can be named.
+    Above language features make functions in Kotlin adjustable.
+    """)
+}
