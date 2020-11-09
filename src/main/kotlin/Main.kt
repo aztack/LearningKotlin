e931613e64d311c2208963531b160888a715cdaa
wangweihua <wangweihua@vipkid.com.cn>
2020-11-09 14:21:57 +0800

    Initial Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+import cookbook.ch1_ranges_progressions_sequences.ch1
+
+fun main() {
+    ch1()
+}
---
+++ src/main/kotlin/cookbook/ch1_ranges_progressions_sequences/ch1.kt
@@ -0,0 +1,9 @@
+package cookbook.ch1_ranges_progressions_sequences
+
+fun ch1() {
+    println("Ch1. Ranges, Progressions, and Sequences")
+    print("""
+    > for (letter in 'Z' downTo 'A') print(letter)")
+    => """)
+    for (letter in 'Z' downTo 'A') print(letter)
+}
