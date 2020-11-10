8dd3cafc253851b1ba8c24437d1b10d823379a7f
wangweihua <wangweihua@vipkid.com.cn>
2020-11-10 16:12:44 +0800

    feat: add book cover

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,7 @@ import cookbook.ch1_ranges_progressions_sequences.ch1
 import cookbook.ch2_expressive_functions_and_adjustable_interfaces.ch2
 
 fun main() {
+    html(img("Kotlin Cookbook Code", "https://user-images.githubusercontent.com/782871/98621983-76eda980-2343-11eb-9cae-54a47db5dd0b.png"))
     ch1()
     ch2()
     q_and_a()
