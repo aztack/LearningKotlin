eadd8a7630d01f211c5c28a63a2b774aab0bc508
wangweihua <wangweihua@vipkid.com.cn>
2020-11-11 10:58:50 +0800

    fix: correct book cover

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,7 @@ import cookbook.ch2_expressive_functions_and_adjustable_interfaces.ch2
 import cookbook.ch3_shaping_code_with_kotlin_functional_programming.ch3
 
 fun main() {
-    html(img("Kotlin Cookbook Code", "https://user-images.githubusercontent.com/782871/98621983-76eda980-2343-11eb-9cae-54a47db5dd0b.png"))
+    html(img("Kotlin Cookbook Code", "https://user-images.githubusercontent.com/782871/98759671-8be23f80-240c-11eb-985c-5fed790c5ffa.png"))
     ch1()
     ch2()
     ch3()
