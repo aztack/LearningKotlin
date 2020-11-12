87c7459fc9e21eadbfeb80c0e05dc3cb5f41d0ea
wangweihua <wangweihua@vipkid.com.cn>
2020-11-12 17:19:56 +0800

    feat: call ch4 in main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,13 @@
 import cookbook.ch1_ranges_progressions_sequences.ch1
 import cookbook.ch2_expressive_functions_and_adjustable_interfaces.ch2
 import cookbook.ch3_shaping_code_with_kotlin_functional_programming.ch3
+import cookbook.ch4_powerful_data_processing.ch4
 
 fun main() {
     html(img("Kotlin Cookbook Code", "https://user-images.githubusercontent.com/782871/98759671-8be23f80-240c-11eb-985c-5fed790c5ffa.png"))
     ch1()
     ch2()
     ch3()
+    ch4()
 }
+
