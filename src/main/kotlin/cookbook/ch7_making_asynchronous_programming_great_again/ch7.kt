26e15c4bb37b2507234ded5254fc64ed22479e61
wangweihua <wangweihua@vipkid.com.cn>
2020-11-12 19:46:20 +0800

    feat: finish first 3 section of ch7

--- src/main/kotlin/cookbook/ch7_making_asynchronous_programming_great_again/ch7.kt
+++ src/main/kotlin/cookbook/ch7_making_asynchronous_programming_great_again/ch7.kt
@@ -1,7 +1,14 @@
 package cookbook.ch7_making_asynchronous_programming_great_again
 
+import code
+import comment
 import h1
 import h2
+import kotlinx.coroutines.*
+import text
+import run
+import kotlin.concurrent.thread
+import kotlin.system.measureTimeMillis
 
 fun ch7() {
     h1("Ch7. Making Asynchronous Programming Great Again")
@@ -17,9 +24,131 @@ fun ch7() {
     ))
 }
 
-fun executing_tasks_in_the_background_using_threads() {}
-fun background_threads_synchronization() {}
-fun using_coroutines_for_asynchronous_concurrent_execution_of_tasks() {}
+fun getCurrentThreadName(): String = Thread.currentThread().name
+fun `5 sec long task` () = Thread.sleep(5000)
+fun `2 sec long task` () = Thread.sleep(2000)
+fun dummyPrint(s: String) = Unit
+
+fun executing_tasks_in_the_background_using_threads() {
+    code("""
+    fun getCurrentThreadName(): String = Thread.currentThread().name
+    fun `5 sec long task` () = Thread.sleep(5000)
+    fun `2 sec long task` () = Thread.sleep(2000)
+    
+    println("Running on ${'$'}{getCurrentThreadName()}")
+    thread {
+        println("Starting async operation on ${'$'}{getCurrentThreadName()}")
+        `5 sec long task`()
+        println("Ending async operation on ${'$'}{getCurrentThreadName()}")
+    }
+    thread {
+        println("Starting async operation on ${'$'}{getCurrentThreadName()}")
+        `2 sec long task`()
+        println("Ending async operation on ${'$'}{getCurrentThreadName()}")
+    }
+    """)
+    dummyPrint("Running on ${getCurrentThreadName()}")
+    thread {
+        dummyPrint("Starting async operation on ${getCurrentThreadName()}")
+        `5 sec long task`()
+        dummyPrint("Ending async operation on ${getCurrentThreadName()}")
+    }
+    thread {
+        dummyPrint("Starting async operation on ${getCurrentThreadName()}")
+        `2 sec long task`()
+        dummyPrint("Ending async operation on ${getCurrentThreadName()}")
+    }
+    run {
+        text("""
+        Running on main
+        Starting async operation on Thread-0
+        Starting async operation on Thread-1
+        Ending async operation on Thread-1
+        Ending async operation on Thread-0
+        """.trimIndent()
+        )
+    }
+}
+
+fun background_threads_synchronization() {
+    code("""
+    println("Running on ${'$'}{getCurrentThreadName()}")
+    thread {
+        println("Starting async operation on ${'$'}{getCurrentThreadName()}")
+        `5 sec long task`()
+        println("Ending async operation on ${'$'}{getCurrentThreadName()}")
+    }.join()
+    thread {
+        println("Starting async operation on ${'$'}{getCurrentThreadName()}")
+        `2 sec long task`()
+        println("Ending async operation on ${'$'}{getCurrentThreadName()}")
+    }.join()
+    """)
+    run {
+        println("Running on ${getCurrentThreadName()}")
+        thread {
+            println("Starting async operation on ${getCurrentThreadName()}")
+            `5 sec long task`()
+            println("Ending async operation on ${getCurrentThreadName()}")
+        }.join()
+        thread {
+            println("Starting async operation on ${getCurrentThreadName()}")
+            `2 sec long task`()
+            println("Ending async operation on ${getCurrentThreadName()}")
+        }.join()
+    }
+}
+private fun `cook rice`() {
+    println("Starting to cook rice on ${getCurrentThreadName()}")
+    Thread.sleep(10000)
+    println("Rice cooked")
+}
+private fun `prepare fish`() {
+    println("Starting to prepare fish on ${getCurrentThreadName()}")
+    Thread.sleep(2000)
+    println("Fish prepared")
+}
+private fun `cut vegetable`() {
+    println("Starting to cut vegetables on ${getCurrentThreadName()}")
+    Thread.sleep(2000)
+    println("Vegetables ready")
+}
+private fun `roll the sushi`() {
+    println("Starting to roll the sushi on ${getCurrentThreadName()}")
+    Thread.sleep(2000)
+    println("Sushi rolled")
+}
+private fun `print current thread name`() {
+    println("Running on ${getCurrentThreadName()}")
+    println()
+}
+fun using_coroutines_for_asynchronous_concurrent_execution_of_tasks() {
+    run {
+        `print current thread name`()
+        var sushiCookingJob: Job
+        measureTimeMillis {
+            sushiCookingJob = GlobalScope.launch(newSingleThreadContext("SushiThread")) {
+                `print current thread name`()
+                var riceCookingJob = GlobalScope.launch {
+                    `cook rice`()
+                }
+                println("Current thread is not blocked while rice is being cooked")
+                `prepare fish`()
+                `cut vegetable`()
+                riceCookingJob.join()
+                `roll the sushi`()
+            }
+            runBlocking {
+                sushiCookingJob.join()
+            }
+        }.also {
+            println("Total time: $it")
+        }
+    }
+    comment("""
+    see [source file](src/main/kotlin/cookbook/ch7_making_asynchronous_programming_great_again/ch7.kt)    
+    """)
+}
 fun using_coroutines_for_asynchronous_concurrent_execution_with_results_handling() {}
 fun applying_coroutines_for_asynchronous_data_processing() {}
 fun easy_coroutine_cancelation() {}
