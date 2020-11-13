bc5b864953be71faf1fffc04477636b3ff8cfcc1
wangweihua <wangweihua@vipkid.com.cn>
2020-11-13 11:43:25 +0800

    feat: add coroutins references and section4

--- src/main/kotlin/cookbook/ch7_making_asynchronous_programming_great_again/ch7.kt
+++ src/main/kotlin/cookbook/ch7_making_asynchronous_programming_great_again/ch7.kt
@@ -4,7 +4,9 @@ import code
 import comment
 import h1
 import h2
+import jdk.nashorn.internal.objects.Global
 import kotlinx.coroutines.*
+import list
 import text
 import run
 import kotlin.concurrent.thread
@@ -12,6 +14,11 @@ import kotlin.system.measureTimeMillis
 
 fun ch7() {
     h1("Ch7. Making Asynchronous Programming Great Again")
+    list(arrayOf(
+        "[Kotlin Reference: Coroutine Basic](https://kotlinlang.org/docs/reference/coroutines/basics.html)",
+        "[KotlinConf 2017 - Deep Dive into Coroutines on JVM by Roman Elizarov](https://www.youtube.com/watch?v=YrrUCSi72E8)",
+        "[Kotlin’s suspend functions compared to JavaScript’s async/await](https://medium.com/@joffrey.bion/kotlins-suspend-functions-are-not-javascript-s-async-they-are-javascript-s-await-f95aae4b3fd9)"
+    ))
     h2(arrayOf(
         ::executing_tasks_in_the_background_using_threads,
         ::background_threads_synchronization,
@@ -101,25 +108,21 @@ fun background_threads_synchronization() {
 private fun `cook rice`() {
     println("Starting to cook rice on ${getCurrentThreadName()}")
     Thread.sleep(1000)
-    Thread.sleep(10000)
     println("Rice cooked")
 }
 private fun `prepare fish`() {
     println("Starting to prepare fish on ${getCurrentThreadName()}")
     Thread.sleep(200)
-    Thread.sleep(2000)
     println("Fish prepared")
 }
 private fun `cut vegetable`() {
     println("Starting to cut vegetables on ${getCurrentThreadName()}")
     Thread.sleep(200)
-    Thread.sleep(2000)
     println("Vegetables ready")
 }
 private fun `roll the sushi`() {
     println("Starting to roll the sushi on ${getCurrentThreadName()}")
     Thread.sleep(200)
-    Thread.sleep(2000)
     println("Sushi rolled")
 }
 private fun `print current thread name`() {
@@ -134,17 +137,6 @@ fun using_coroutines_for_asynchronous_concurrent_execution_of_tasks() {
             `print current thread name`()
             var riceCookingJob = GlobalScope.launch {
                 `cook rice`()
-        measureTimeMillis {
-            sushiCookingJob = GlobalScope.launch(newSingleThreadContext("SushiThread")) {
-                `print current thread name`()
-                var riceCookingJob = GlobalScope.launch {
-                    `cook rice`()
-                }
-                println("Current thread is not blocked while rice is being cooked")
-                `prepare fish`()
-                `cut vegetable`()
-                riceCookingJob.join()
-                `roll the sushi`()
             }
             println("Current thread is not blocked while rice is being cooked")
             `prepare fish`()
@@ -158,14 +150,52 @@ fun using_coroutines_for_asynchronous_concurrent_execution_of_tasks() {
             }
         }.also {
             println("Total time: $it ms")
-            println("Total time: $it")
         }
     }
     comment("""
     see [source file](src/main/kotlin/cookbook/ch7_making_asynchronous_programming_great_again/ch7.kt)    
     """)
 }
-fun using_coroutines_for_asynchronous_concurrent_execution_with_results_handling() {}
+
+private suspend fun `caculate the anwser to life the universe and everything`(): Int {
+    delay(5000)
+    return 42
+}
+private suspend fun `show progress animation`() {
+    val progressBarLength = 30
+    var currentPosition = 0
+    while (true) {
+        print("\r")
+        val progressbar = (0 until progressBarLength).map {if (it == currentPosition) ">" else "-"}.joinToString("")
+        print(progressbar)
+        delay(50)
+        if (currentPosition == progressBarLength) currentPosition = 0
+        currentPosition++
+    }
+}
+
+fun using_coroutines_for_asynchronous_concurrent_execution_with_results_handling() {
+    comment("""
+    see [source file](src/main/kotlin/cookbook/ch7_making_asynchronous_programming_great_again/ch7.kt)
+    """)
+    run {
+        `print current thread name`()
+        GlobalScope.launch {
+            println("Starting progressbar animation on ${getCurrentThreadName()}")
+            `show progress animation`()
+        }
+        val future = GlobalScope.async {
+            println("Starting computations on ${getCurrentThreadName()}")
+            `caculate the anwser to life the universe and everything`()
+        }
+        println("\n${getCurrentThreadName()} thread is not blocked while tasks are in progress")
+
+        runBlocking {
+            println("\nThe anwser to life the universe and everything: ${future.await()}")
+            `print current thread name`()
+        }
+    }
+}
 fun applying_coroutines_for_asynchronous_data_processing() {}
 fun easy_coroutine_cancelation() {}
 fun building_a_REST_API_client_with_Retrofit_and_coroutines_adapter() {}
