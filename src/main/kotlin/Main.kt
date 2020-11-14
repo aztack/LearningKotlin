cf5e692786f8bb761a28cae15b76947daf1e6a3f
wangweihua <aztack@163.com>
2020-11-14 15:28:06 +0800

    feat: add ch8(not implemented)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,6 +5,7 @@ import cookbook.ch4_powerful_data_processing.ch4
 import cookbook.ch5_tasteful_design_patterns_adopting_kotlin_concepts.ch5
 import cookbook.ch6_friendly_io_operations.ch6
 import cookbook.ch7_making_asynchronous_programming_great_again.ch7
+import cookbook.ch8_best_practices_for_the_android_junit_and_jvm_ui_frameworks.ch8
 
 fun main() {
     html(img("Kotlin Cookbook Code", "https://user-images.githubusercontent.com/782871/98759671-8be23f80-240c-11eb-985c-5fed790c5ffa.png"))
@@ -15,5 +16,6 @@ fun main() {
     ch5()
     ch6()
     ch7()
+    ch8()
 }
 
---
+++ src/main/kotlin/cookbook/ch8_best_practices_for_the_android_junit_and_jvm_ui_frameworks/ch8.kt
@@ -0,0 +1,7 @@
+package cookbook.ch8_best_practices_for_the_android_junit_and_jvm_ui_frameworks
+
+import not_implemented
+
+fun ch8() {
+    not_implemented()
+}
