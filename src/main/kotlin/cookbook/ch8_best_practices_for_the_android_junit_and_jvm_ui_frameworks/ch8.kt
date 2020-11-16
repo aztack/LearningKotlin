2bfa311dffad9f37f6792c6fdf27f0fe9a005964
wangweihua <wangweihua@vipkid.com.cn>
2020-11-16 19:15:19 +0800

    fix: chapter 8 title

--- src/main/kotlin/cookbook/ch8_best_practices_for_the_android_junit_and_jvm_ui_frameworks/ch8.kt
+++ src/main/kotlin/cookbook/ch8_best_practices_for_the_android_junit_and_jvm_ui_frameworks/ch8.kt
@@ -4,6 +4,6 @@ import h1
 import not_implemented
 
 fun ch8() {
-    h1("Best Practices for the Android JUnit and JVM UI Frameworks")
+    h1("Ch8. Best Practices for the Android JUnit and JVM UI Frameworks")
     not_implemented()
 }
