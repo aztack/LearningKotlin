fac1a50ae2239f0002f7bb0b3314168316bea342
wangweihua <wangweihua@vipkid.com.cn>
2020-11-16 19:05:45 +0800

    feat: add title for ch8

--- src/main/kotlin/cookbook/ch8_best_practices_for_the_android_junit_and_jvm_ui_frameworks/ch8.kt
+++ src/main/kotlin/cookbook/ch8_best_practices_for_the_android_junit_and_jvm_ui_frameworks/ch8.kt
@@ -1,7 +1,9 @@
 package cookbook.ch8_best_practices_for_the_android_junit_and_jvm_ui_frameworks
 
+import h1
 import not_implemented
 
 fun ch8() {
+    h1("Best Practices for the Android JUnit and JVM UI Frameworks")
     not_implemented()
 }
